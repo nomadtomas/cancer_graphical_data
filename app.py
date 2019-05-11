32550eb395fbc0e39963f332d74fb8a0afabd95f
Tomas Resendiz <l2tt2lresendiz@gmail.com>
2019-05-10 19:12:49 -0500

    added app.py

---
+++ app.py
@@ -0,0 +1,69 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+
+#################################################
+# Database Setup
+#################################################
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+Samples_Metadata = Base.classes.sample_metadata
+Samples = Base.classes.samples
+
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+
+@app.route("/names")
+def names():
+    """Return a list of sample names."""
+
+    # Use Pandas to perform the sql query
+    stmt = db.session.query(Samples).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    # Return a list of the column names (sample names)
+    #return jsonify(list(df.columns)[2:])
+
+
+@app.route()
+def sample_metadata(sample):
+    """Return the MetaData for a given sample."""
+   
+    # Create a dictionary entry for each row of metadata information
+   
+    return jsonify(sample_metadata)
+
+
+@app.route()
+def samples(sample):
+   
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run()
